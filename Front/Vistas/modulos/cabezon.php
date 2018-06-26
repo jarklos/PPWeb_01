b37de50d1b7d6f32313c5a6e6eb2006e1e5dd83f
ManuFerrer <manuferrerhectormartinez@gmail.com>
2018-06-26 10:29:40 +0200

    Dia dos F&B
    Primera Version Estable del Front y Back

---
+++ Front/Vistas/modulos/cabezon.php
@@ -0,0 +1,33 @@
+<!-- Declaramos el top -->
+
+<div class ="container-fluid BarraSuperior" id = "top">
+	<div class="container">
+		<div class ="row">
+			<!-- Social -->
+			<div class ="col-lg-9 col-md-9 col-sm-8 col-xs-12">
+				<ul>
+					<li>
+						<a href = "https://facebook.com/" target="_blank">
+							
+							<i class="fab fa-facebook-square redSocial facebookBlanco" aria-hidden ="true""></i>
+						</a>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+			<!-- registro -->
+			<div class ="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+				<ul>
+					<li><a href ="#modalIngreso" data-toggle = "modal">Ingresar</a></li>
+					<li>|</li>
+					<li><a href ="#modalRegistro" data-toggle = "modal">Crear una cuenta</a></li>
+				</ul>
+			</div>
+			
+
+		</div>
+	</div>
+</div>
--- Front/Vistas/plantilla.php
+++ Front/Vistas/plantilla.php
@@ -2,10 +2,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<!-- Definimos los caracteres de España-->
 	<meta charset="UTF-8">
+	<!-- Ajustamos para que sea responsive-->
+	<meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale = 1.0,maximum-scale =1.0,user-scalable=no">
+	<!-- Aqui comienza el header(Cabezon)-->
+	<meta name = "Titulo" content ="Psiky Formacion">
+
+	<meta name = "descripcion" content ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsa ea maxime mollitia vitae voluptates quod at saepe reprehenderit totam aliquam architecto fugiat,">
+
+	<meta name = "keywords" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsa ea maxime mollitia vitae voluptates quod at saepe reprehenderit totam aliquam architecto fugiat, sunt cumque consequatur obcaecati laudantium illo ratione?">
+
+	<!-- Aqui termina el header (Cabezon)-->
+
 	<title>Psikers</title>
+
+	<!-- Importamos Librerias-->
+
+
+	<link rel="stylesheet"  href="vistas/css/plugin/bootstrap/css/bootstrap.min.css">
+
+	<link rel="stylesheet"  href="vistas/css/plugin/font-awesome/css/font-awesome.min.css">
+
+	<script src = vistas/js/plugins/jquery.js></script>
+
+	<script src = vistas/js/plugins/bootstrap.min.js></script>
+
+
 </head>
 <body>
-	<hi>Psikers</hi>
+	
+	<?php 
+		include "modulos/cabezon.php";
+	 ?>
+	
 </body>
 </html>
