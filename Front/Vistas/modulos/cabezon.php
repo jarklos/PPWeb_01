9e5f36724b9cbe2cf69528707aec868eccfa6c4f
ManuFerrer <manuferrerhectormartinez@gmail.com>
2018-06-26 13:42:21 +0200

    Commit final del dia

--- Front/Vistas/modulos/cabezon.php
+++ Front/Vistas/modulos/cabezon.php
@@ -4,26 +4,84 @@
 	<div class="container">
 		<div class ="row">
 			<!-- Social -->
-			<div class ="col-lg-9 col-md-9 col-sm-8 col-xs-12">
+			<!--Insterte enlace aqui -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1">
 				<ul>
 					<li>
 						<a href = "https://facebook.com/" target="_blank">
-							
-							<i class="fab fa-facebook-square redSocial facebookBlanco" aria-hidden ="true""></i>
+							<i class="fab fa-facebook-f"></i>
 						</a>
-
-
 					</li>
-
 				</ul>
-
 			</div>
+			<!--Insterte enlace aqui -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1"">
+				<ul>
+					<li>
+						<a href = "https://facebook.com/" target="_blank">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!--Insterte enlace aqui -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1"">
+				<ul>
+					<li>
+						<a href = "https://facebook.com/" target="_blank">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!--Insterte enlace aqui -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1"">
+				<ul>
+					<li>
+						<a href = "https://facebook.com/" target="_blank">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!--Insterte enlace aqui -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1"">
+				<ul>
+					<li>
+						<a href = "https://facebook.com/" target="_blank">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!--Insterte enlace aqui -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1"">
+				<ul>
+					<li>
+						<a href = "https://facebook.com/" target="_blank">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!--Insterte enlace aqui -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1"">
+				<ul>
+					<li>
+						<a href = "https://facebook.com/" target="_blank">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- Casilla Vacia -->
+			<div class ="col-lg-1 col-md-1 col-sm-1 col-xs-1""></div>
 
 			<!-- registro -->
-			<div class ="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+			<div class ="col-lg-4 col-md-4 col-sm-4 col-xs-12 registro">
 				<ul>
 					<li><a href ="#modalIngreso" data-toggle = "modal">Ingresar</a></li>
-					<li>|</li>
+					
 					<li><a href ="#modalRegistro" data-toggle = "modal">Crear una cuenta</a></li>
 				</ul>
 			</div>
@@ -36,34 +94,114 @@
 		<div class="container">
 			<div class ="row" id = "cabezon">
 				<!-- Logotipo-->
-				<div class ="col-lg-3 col-md-3 col-sm-3 col-xs-12" id="logotipo">
+				<div class ="col-lg-2 col-md-2 col-sm-2 col-xs-12" id="logotipo">
 					<a href ="#">
-						<img src="http://localhost/ppweb_01/PPWeb_01/Back/vistas/Img/logo_test.png" class ="img-responsive">
+						<img src="http://localhost/ppweb_01/PPWeb_01/Back/vistas/Img/psiky_logo.jpg" class ="img-responsive">
 					</a>
 				</div>
 				<!-- Categorias-->
 				<div class = "col-lg-6 col-md-6 col-sm-8 col-xs-12">
-					<div class = "col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id = "botonCategoria">
+					<div class = "col-lg-3 col-md-3 col-sm-3 col-xs-12 backColor" id = "botonCategoria">
 						<p>Categoria
 							<span class ="pull-right"><i class="fa fa-bars" aria-hidden ="true"></i></span>
 						</p>
 						
-					</div>
-					<!-- Buscador-->
-					<div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
-						<input type="search" name="buscar" class="form-control" placeholder="Buscar...">
-						
-						<span class="input-group-btn">
-							<a href="#">
-								<button class="btn btn-default backColor" type="submit">
-									<i class="fa fa-serch"></i>
-								</button>
-							</a>
-						</span>
-					</div>
 				</div>
-				
+				<!-- Categorias-->
+				<!-- Buscador-->
+				<div class="input-group col-lg-6 col-md-6 col-sm-6 col-xs-12" id="buscador">
+					<input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+					<span class="input-group-btn">
+						<a href="#">
+							<button class="btn btn-default backColor" type="submit">
+								<i class="fa fa-serch"></i>
+							</button>
+						</a>
+					</span>
+				</div>
+						
+				<!-- carro-->
+				<div class="col-lg-3 col-md-2 col-sm-2 col-xs-12" id="carrito">
+					<a href="#">
+						<button class ="btn btn-default pull-left backColor">
+							
+							<i class ="fa fa-shopping-cart" aria-hidden ="true"></i>
+						</button>
+					</a>
+					<p>Tu Cesta <span class="cantidadCesta"></span><br>Euros €<span class="sumaCesta"></span></p>
+
+				</div>
+				<!-- carro-->
+
 			</div>
+				
+		</div>
+			<!-- Categorias y subcategorias-->
+<!-- Aqui se crea una categoria-->
+<div class="row" id="categorias">
+	<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<h4>
+			<a href="#" class="pixelCategorias">Psicologia Clinica</a>
+		</h4>
+		<hr>
+		<ul>
+			<!--Aqui se definen las subcategorias-->
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+		</ul>
+</div>
+<!-- Aqui se crea una categoria-->
+	<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<h4>
+			<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+		</h4>
+		<hr>
+		<ul>
+			<!--Aqui se definen las subcategorias-->
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+		</ul>
+	</div>
+<!-- Aqui se crea una categoria-->
+<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<h4>
+			<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+		</h4>
+		<hr>
+		<ul>
+			<!--Aqui se definen las subcategorias-->
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+		</ul>
+	</div>
+
+<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<h4>
+			<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+		</h4>
+		<hr>
+		<ul>
+			<!--Aqui se definen las subcategorias-->
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+			<li><a href="#" class= "pixelSubCategoria">Lorem Ipsum</a></li>
+		</ul>
+	</div>
+
+
+</div>
+<!-- Categorias y subcategorias-->
+
 		</div>
 		
 	</header>
