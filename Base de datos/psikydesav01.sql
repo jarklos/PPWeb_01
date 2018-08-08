9d7a4e0ae81eda0ead3d31ac3b5ccacf40a8ea95
presenciavr <33731076+presenciavr@users.noreply.github.com>
2018-08-08 18:27:17 +0200

    Cambio nombre archivo SQL
    Cambio nombre archivo SQL

--- Base de datos/psikydesav01.sql
+++ Base de datos/psikydesav01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-08-2018 a las 01:12:36
+-- Tiempo de generación: 08-08-2018 a las 18:26:10
 -- Versión del servidor: 10.1.34-MariaDB
 -- Versión de PHP: 7.2.7
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `psikydesa`
+-- Base de datos: `psikydesav01`
 --
 
 -- --------------------------------------------------------
