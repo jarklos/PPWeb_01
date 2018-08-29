22d9279c25fb8e6a99db7c2889a1ae3e8a5af74e
pa7ry <pa7ry>
2018-08-29 21:40:35 +0200

    añadidos

--- Formacion/src/app/components/user/user-data/user-data.component.ts
+++ Formacion/src/app/components/user/user-data/user-data.component.ts
@@ -20,20 +20,20 @@ export class UserDataComponent implements OnInit {
   public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType:string = 'bar';
   public barChartLegend:boolean = true;
- 
+
   public barChartData:any[] = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
   ];
- 
+
   // events
   public chartClicked(e:any):void {
     console.log(e);
   }
- 
+
   public chartHovered(e:any):void {
     console.log(e);
   }
- 
+
   public randomize():void {
     // Only Change 3 values
     let data = [
